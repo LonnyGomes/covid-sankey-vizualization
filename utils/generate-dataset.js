638a38b30254aae0067d82ce66f1eb285957d2b9
Lonny Gomes <code@lonnygomes.com>
2020-03-27 13:53:03 -0400

    wip: build starter script that outputs JSON to STDOUT
    This is the first of many steps but in the meantime, it's enough  to  update the data
    
    refs #49

---
+++ utils/generate-dataset.js
@@ -0,0 +1,58 @@
+const allCountriesEndpoint = 'https://corona.lmao.ninja/v2/jhucsse';
+const axios = require('axios').default;
+const GLOBALS = {
+    US_KEY: 'US',
+};
+const parseData = data => {
+    const OBJECT_KEYS = ['confirmed', 'deaths', 'recovered'];
+    const results = {};
+    const usaResults = {};
+
+    data.forEach(item => {
+        if (!results[item.country]) {
+            results[item.country] = {};
+            for (const curKey of OBJECT_KEYS) {
+                results[item.country][curKey] = 0;
+            }
+            results[item.country]['date'] = item.updatedAt;
+        }
+
+        // handle USA data
+        if (item.country === GLOBALS.US_KEY) {
+            usaResults[item.province] = [
+                {
+                    date: item.updatedAt,
+                },
+            ];
+        }
+
+        const countryObj = results[item.country];
+
+        for (const curKey of OBJECT_KEYS) {
+            const curVal = item.stats[curKey];
+            countryObj[curKey] += isNaN(curVal) ? 0 : Number(curVal);
+
+            if (item.country === GLOBALS.US_KEY) {
+                usaResults[item.province][0][curKey] = isNaN(curVal)
+                    ? 0
+                    : Number(curVal);
+            }
+        }
+    });
+
+    //console.log('usaResults', usaResults);
+    const final = Object.keys(results).reduce((obj, curKey) => {
+        obj[curKey] = [results[curKey]];
+        return obj;
+    }, {});
+
+    return { world: final, us: usaResults };
+};
+
+axios
+    .get(allCountriesEndpoint)
+    .then(response => response.data)
+    .then(data => {
+        const results = parseData(data);
+        console.log(JSON.stringify(results, null, 2));
+    });

1024eaa70da7b16af1fd29bd7055708cb1e9c10e
Lonny Gomes <code@lonnygomes.com>
2020-04-01 21:43:09 -0400

    feat: replace us data source to use novelcovid api
    refs #74

--- utils/generate-dataset.js
+++ utils/generate-dataset.js
@@ -54,7 +54,7 @@ const parseJHUeData = data => {
     return { world: final, us: usaResults };
 };
 
-const parseNovelData = data => {
+const parseNovelCountryData = data => {
     const OBJECT_KEYS = ['confirmed', 'active', 'deaths', 'recovered'];
     const results = {};
 
@@ -86,21 +86,53 @@ const parseNovelData = data => {
     return final;
 };
 
+const parseNovelStateData = data => {
+    const OBJECT_KEYS = ['confirmed', 'active', 'deaths'];
+    const results = {};
+    data.forEach(item => {
+        const primaryKey = item.country || item.state;
+
+        if (!results[primaryKey]) {
+            results[primaryKey] = {};
+            for (const curKey of OBJECT_KEYS) {
+                results[primaryKey][curKey] = 0;
+            }
+            results[primaryKey]['date'] = item.updated;
+        }
+
+        const countryObj = results[primaryKey];
+
+        for (const curKey of OBJECT_KEYS) {
+            const key = curKey === 'confirmed' ? 'cases' : curKey;
+            const curVal = item[key];
+            countryObj[curKey] += isNaN(curVal) ? 0 : Number(curVal);
+        }
+
+        // calculate the recovered value given the active and deaths of all cases
+        countryObj.recovered =
+            countryObj.confirmed - countryObj.active - countryObj.deaths;
+    });
+
+    const final = Object.keys(results).reduce((obj, curKey) => {
+        obj[curKey] = [results[curKey]];
+        return obj;
+    }, {});
+
+    return final;
+};
+
 Promise.all([
     // retrieve latest up to date world data
-    covid.getCountry().then(parseNovelData),
-    // retrieve JHU data for the US
-    axios
-        .get(allCountriesEndpoint)
-        .then(response => response.data)
-        .then(parseJHUeData),
+    covid.getCountry().then(parseNovelCountryData),
+    // latest up to date world data
+    covid.getState().then(parseNovelStateData),
 ])
     .then(data => {
-        const [world, { us }] = data;
+        const [world, us] = data;
 
         const dataStr = JSON.stringify({ world, us }, null, 2);
         console.log(dataStr);
     })
     .catch(err => {
-        console.error(err.message);
+        console.error('error', err.message);
     });

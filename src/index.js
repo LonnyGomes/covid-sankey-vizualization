c70c643887b444e0c0806bb58e62da126eaf0dda
Lonny Gomes <code@lonnygomes.com>
2020-04-06 18:24:36 -0400

    feat: implement offline caching in service worker
    refs #82

--- src/index.js
+++ src/index.js
@@ -14,26 +14,29 @@ let isUSSelected = false;
 
 const initServiceWorker = () => {
     if ('serviceWorker' in navigator) {
-        //window.addEventListener('load', () => {
-        navigator.serviceWorker
-            .register('service-worker.js')
-            .then(
-                (registration) => {
-                    // Registration was successful
-                    console.log(
-                        'ServiceWorker registration successful with scope: ',
-                        registration.scope
-                    );
-                },
-                (err) => {
-                    // registration failed :(
-                    console.log('ServiceWorker registration failed: ', err);
-                }
-            )
-            .catch((err) => {
-                console.log(err);
-            });
-        //});
+        window.addEventListener('load', () => {
+            navigator.serviceWorker
+                .register('service-worker.js')
+                .then(
+                    (registration) => {
+                        // Registration was successful
+                        console.log(
+                            '[Service Worker] registration successful with scope: ',
+                            registration.scope
+                        );
+                    },
+                    (err) => {
+                        // registration failed :(
+                        console.log(
+                            '[Service Worker] registration failed: ',
+                            err
+                        );
+                    }
+                )
+                .catch((err) => {
+                    console.log(err);
+                });
+        });
     } else {
         console.log('service worker is not supported');
     }
